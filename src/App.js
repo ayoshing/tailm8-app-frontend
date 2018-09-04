ad5bbe0cb781a55cb1f84b9b9917d75d440c016f
shing <shing2487@gmail.com>
2018-09-04 02:07:56 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component, Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
-import Header from './components/Header';
-import indexRoutes from './routes/index.js';
+import Header from "./components/Header";
+import indexRoutes from "./routes/index.js";
 
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {}
+
   render() {
     return (
       <Fragment>
         <Header />
         <Switch>
           {indexRoutes.map((prop, key) => {
-            return <Route path={prop.path} key={key} component={prop.component} />;
+            return (
+              <Route path={prop.path} key={key} component={prop.component} />
+            );
           })}
         </Switch>
       </Fragment>
--- src/apis/UserAdapter.js
+++ src/apis/UserAdapter.js
@@ -2,4 +2,12 @@ export default class UserAdapter {
   static getToken() {
     return localStorage.jwt;
   }
+
+  static clearToken() {
+    return localStorage.removeItem("jwt");
+  }
+
+  static isLoggedIn() {
+    return !!UserAdapter.getToken();
+  }
 }
