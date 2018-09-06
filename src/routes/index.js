fb7d159e4a97a9a0d426c0407952cefc194db2c7
Shing Wong <shing2487@gmail.com>
2018-09-06 14:22:35 -0400

    updated routes to include a test route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import SignUpPage from "../views/SignUpPage";
 import MainPage from "../views/MainPage";
 import ProfileForm from "../components/ProfileForm";
+import TestPage from "../views/TestPage";
 
 const indexRoutes = [
   { path: "/signup", name: "SignUpPage", component: SignUpPage },
   { path: "/profile/edit", name: "ProfileForm", component: ProfileForm },
+  { path: "/test", name: "TestPage", component: TestPage },
   { path: "/", name: "MainPage", component: MainPage }
 ];
 
